0ceaace4217d74adba61f93b4ea3565ca75d1507
naomyxpro <94860461+naomyxpro@users.noreply.github.com>
2023-10-30 15:45:58 -0400

    ordena mes de menor a mayor
    en la lista del servicio

--- FRONT/src/Servicios/components/ServicioList.jsx
+++ FRONT/src/Servicios/components/ServicioList.jsx
@@ -10,23 +10,32 @@ export const ServicioList = () => {
   const formatDate = (dateString) => {
     const date = new Date(dateString);
     date.setDate(date.getDate() + 1); // Suma un día para corregir la diferencia
-  
+
     return format(date, 'dd MMMM', { locale: esLocale });
   };
 
   const [servicios, setServicio] = useState([]);
+
+  const sortAndSetServicios = (data) => {
+    const serviciosFormateados = data.map((servicio) => ({
+      ...servicio,
+      fechaInicio: formatDate(servicio.fechaInicio),
+      fechaFin: formatDate(servicio.fechaFin),
+      mes: new Date(servicio.fechaInicio).getMonth(),
+    }));
+
+    // Ordenar los servicios por mes de menor a mayor
+    serviciosFormateados.sort((a, b) => a.mes - b.mes);
+
+    setServicio(serviciosFormateados);
+  };
 
   const getListServicio = async () => {
     const data = await getAllServicios();
     if (data === "error") {
       return <Link to="../../">Volver a la página principal</Link>;
     } else {
-      const serviciosFormateados = data.map((servicio) => ({
-        ...servicio,
-        fechaInicio: formatDate(servicio.fechaInicio),
-        fechaFin: formatDate(servicio.fechaFin),
-      }));
-      setServicio(serviciosFormateados);
+      sortAndSetServicios(data);
     }
   };
 
@@ -58,7 +67,7 @@ export const ServicioList = () => {
                 <h4 className="card-title font-weight-bold">{servicio.nombre}</h4>
                 <p className="card-text small" style={{ fontSize: '10px' }}>{servicio.descripcion}</p>
                 <hr />
-                <p className="card-text small" >Cupos: <strong style={{ fontSize: '17px', color: 'green' }}>{servicio.cupo}</strong></p>
+                <p className="card-text small">Cupos: <strong style={{ fontSize: '17px', color: 'green' }}>{servicio.cupo}</strong></p>
                 <p className="card-text small">Precio: {servicio.precio}Bs</p>
                 <div className="d-flex justify-content-between align-items-center">
                   <Link to={`/servicios/edit/${servicio.id}`} className="btn btn-primary small">
